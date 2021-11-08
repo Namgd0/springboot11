c34cbb74fd4e47adc038fc056ac2d2c43811017e
nam <namgd69@gmail.com>
2021-11-08 09:55:43 +0700

    du an springboot

---
+++ src/main/java/com/example/springboot11/Springboot11Application.java
@@ -0,0 +1,41 @@
+package com.example.springboot11;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+public class Springboot11Application {
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(Springboot11Application.class, args);
+        UserRepository userRepository = context.getBean(UserRepository.class);
+
+        System.out.println("Tìm user với Agi trong khoảng (25 - 30)");
+        System.out.println("findAllByAgiBetween: ");
+        userRepository.findAllByAgiBetween(25, 30)
+                .forEach(System.out::println);
+
+        System.out.println("===========================================");
+        System.out.println("Tìm user với Agi trong lớn hơn 97");
+        System.out.println("findAllByAgiGreaterThan: ");
+        userRepository.findAllByAgiGreaterThan(97)
+                .forEach(System.out::println);
+
+        // Tìm kiếm tất cả theo Atk = 74
+        System.out.println("===========================================");
+        System.out.println("Tìm user với Atk = 74");
+        System.out.println("findAllByAtk: ");
+        userRepository.findAllByAtk(74)
+                .forEach(System.out::println);
+
+        System.out.println("===========================================");
+        System.out.println("Tìm User với def = 49");
+        System.out.println("SELECT u FROM User u WHERE u.def = :def");
+        User user = userRepository.findUserByDefQuery(49);
+        System.out.println("User: " + user);
+    }
+
+}
